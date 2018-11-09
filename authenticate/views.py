0bcfdbf11828f1ef559b5eaa2e290520845be963
Deepayan Mallick <deepayan1@live.com>
2018-11-09 11:05:49 +0600

    adding forms.py and register page

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from . forms import SignUpForm
 
 def home(request):
     return render(request, 'authenticate/home.html', {})
@@ -11,10 +14,31 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, f'You Have Been Logged in!')
             return redirect('home')
         else:
+            messages.warning(request, f'Error Logging In - Please Try Again...')
             return redirect('login')
     else:
         return render(request, 'authenticate/login.html', {})
 
+def logout_user(request):
+    logout(request)
+    messages.warning(request, f'You Have Been Logged Out! Please Login To stay connected...')
+    return redirect('login')
 
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f'You Have Been Registered Successfully!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    context = {'form': form}
+    return render(request, 'authenticate/register.html', context)
